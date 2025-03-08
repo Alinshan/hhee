8581fc645f54164793d0baa0d00017075b2bfcb7
shaan.403 <86637047+Alinshan@users.noreply.github.com>
2025-03-08 21:11:18 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import CheckIcon from "@/components/icons/check-icon";
 import GithubIcon from "@/components/icons/github-icon";
-import PictureIcon from "@/components/icons/picture-icon";
 import XIcon from "@/components/icons/x-icon";
 import Logo from "@/components/logo";
 import Spinner from "@/components/spinner";
